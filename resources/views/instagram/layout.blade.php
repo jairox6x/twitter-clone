36a31185623051881282150f314203524cec6278
Jairo Rodriguez <bethelcenter@gmail.com>
2020-04-23 20:27:49 -0400

    added  image

--- resources/views/instagram/layout.blade.php
+++ resources/views/instagram/layout.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body class="bg-gray-100">
 
-<div class="w-full bg-white border-b border-gray-400">
+<div class="w-full bg-white border-b border-gray-400 sticky top-0">
     <div class="container mx-auto py-2 px-32">
         <nav class="flex justify-between items-center">
             <div class="logo">
@@ -62,7 +62,7 @@
 
         <div class="flex">
             <section class="feed w-2/3">
-                <article class="flex-col border border-gray-400">
+                <article class="flex-col border border-gray-400 rounded-sm">
                     <header class="border flex justify-between px-3 py-2">
                         <div class="user-info flex">
                             <img src="{{random_avatar(50)}}" class="rounded-full" alt="user-avatar">
@@ -72,25 +72,27 @@
                             </div>
                         </div>
                     </header>
-                    <main>Carne</main>
+                    <main>
+                        <img src="https://source.unsplash.com/random">
+                    </main>
                     <footer class="mt-2">
-                        <div class="flex">
-                            <div class="share">
-                                <a href="#">
+                        <div class="flex justify-between px-4 py-2">
+                            <div class="share flex">
+                                <a href="#" class="">
                                     <svg aria-label="Me gusta" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
                                         <path clip-rule="evenodd"
                                               d="M34.3 3.5C27.2 3.5 24 8.8 24 8.8s-3.2-5.3-10.3-5.3C6.4 3.5.5 9.9.5 17.8s6.1 12.4 12.2 17.8c9.2 8.2 9.8 8.9 11.3 8.9s2.1-.7 11.3-8.9c6.2-5.5 12.2-10 12.2-17.8 0-7.9-5.9-14.3-13.2-14.3zm-1 29.8c-5.4 4.8-8.3 7.5-9.3 8.1-1-.7-4.6-3.9-9.3-8.1-5.5-4.9-11.2-9-11.2-15.6 0-6.2 4.6-11.3 10.2-11.3 4.1 0 6.3 2 7.9 4.2 3.6 5.1 1.2 5.1 4.8 0 1.6-2.2 3.8-4.2 7.9-4.2 5.6 0 10.2 5.1 10.2 11.3 0 6.7-5.7 10.8-11.2 15.6z"
                                               fill-rule="evenodd"></path>
                                     </svg>
                                 </a>
-                                <a href="#">
+                                <a href="#" class="ml-4">
                                     <svg aria-label="Comentar" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
                                         <path clip-rule="evenodd"
                                               d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z"
                                               fill-rule="evenodd"></path>
                                     </svg>
                                 </a>
-                                <a href="#">
+                                <a href="#" class="ml-4">
                                     <svg aria-label="Compartir publicación" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24">
                                         <path d="M46.5 3.5h-45C.6 3.5.2 4.6.8 5.2l16 15.8 5.5 22.8c.2.9 1.4 1 1.8.3L47.4 5c.4-.7-.1-1.5-.9-1.5zm-40.1 3h33.5L19.1 18c-.4.2-.9.1-1.2-.2L6.4 6.5zm17.7 31.8l-4-16.6c-.1-.4.1-.9.5-1.1L41.5 9 24.1 38.3z"></path>
                                         <path d="M14.7 48.4l2.9-.7"></path>
